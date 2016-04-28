b416d5e7dd7de942aad8b6b0f9d0ffca0f253212
meltzwho <meltzer.eric@live.com>
2016-04-27 22:27:26 -0400

    CombinationLock class - working

---
+++ Practice/src/CombinationLock.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class CombinationLock {
+	LinkedList<Character> lock=new LinkedList<>();
+	public CombinationLock(){
+		buildLock();
+	}
+	//methods
+	private void buildLock(){
+		for(int i=0;i<39;i++){
+			lock.addLast('-');
+		}
+	}
+	private void displayLock(){
+		
+	}
+	public void moveLock(boolean turn, int amt){
+		if(turn){
+			
+		}else{
+			
+		}
+	}
+}
