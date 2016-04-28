621a0454f3c544c91bf0e827e92d5c8651278280
meltzwho <meltzer.eric@live.com>
2016-04-28 11:48:09 -0400

    Improved print method - possible bug on main testcase

--- Practice/src/CombinationLock.java
+++ Practice/src/CombinationLock.java
@@ -4,78 +4,70 @@ public class CombinationLock {
 	//fields
 	LinkedList<Character> lock=new LinkedList<>();
 	LinkedList<Integer> nums=new LinkedList<>();
-	boolean whichway;
 	//constructor
 	public CombinationLock(){
 		buildLock(true);
-		displayLock();
+		displayLock(false);
 	}
 	//methods
 	private void buildLock(boolean init){
 		lock.clear();
-		for(int i=0;i<39;i++){
+		for(int i=0;i<40;i++){
 			lock.add('-');
 			if(init)
 				nums.add(i);
 		}
 	}
-	private void displayLock(){
-		int count=0;
-		int count2=nums.size();
-			System.out.println("            " + nums.getFirst());
-			for(int i=0; i <10 ; i++){
-		    		for (int j = 12; j>0+i ; j--){
-					System.out.print(" ");
-		    		}
-		    		if(i==0&&!whichway){
-			    		System.out.print(lock.get(++count));
-		    		}else{
-		    		System.out.print(lock.get(--count2));
-		    		}
-		    		if(i == 0)
-			 		System.out.println();
-		    		if(i!=0){
-					for(int j= 1; j< 2*i; j++){
-			    			System.out.print(" ");
-					}
-				System.out.println(lock.get(++count));
-		    		}
+	private void displayLock(boolean turn){
+		int head=0;
+		int tail=nums.size()-1;
+		//Prints top to mid-line
+		System.out.println("            "+nums.getFirst());
+		for(int i=0;i<10;i++){
+		    for (int j=12;j>i; j--){
+				System.out.print(" ");
+		    	}
+		    if(i==0&&!turn)
+			    System.out.print(lock.get(head++));
+		    else
+		    	System.out.print(lock.get(tail--));
+		    if(i==0)
+			 	System.out.println();
+		    if(i!=0){
+				for(int j= 1; j< 2*i; j++){
+			    	System.out.print(" ");
+				}
+			System.out.println(lock.get(head++));
 			}
-	    	
-	    
-	    	//Postcondition: prints the middle line
-			
-			if(whichway)
-				System.out.print(nums.get(++count2)+""+ lock.get(--count2));
-			else
-				System.out.print(nums.get(count2)+""+ lock.get(--count2));
-			for(int i = 0; i<19; i++){
-		    		System.out.print(" ");
+		}
+		//Prints mid-line
+		if(turn)
+			System.out.print(nums.get(++tail)+""+ lock.get(tail));
+		else
+			System.out.print(nums.get(tail)+""+ lock.get(tail));
+		for(int i = 0; i<19; i++){
+			System.out.print(" ");
+		}
+		if(turn)
+			System.out.println(lock.get(++head) +""+ nums.get(head));
+		else
+			System.out.println(lock.get(head) +""+ nums.get(head));
+		//Print from mid-line to bottom
+		for(int i=0; i<10; i++){
+		    for (int j= i+3; j>0; j--){
+				System.out.print(" ");
+		    }
+		    if(i!=9)
+		    	System.out.print(lock.get(--tail));
+		    for (int j=18-(2*i); j>1;j--){
+				System.out.print(" ");
+		    }
+		    System.out.println(lock.get(++head));
 			}
-			if(whichway)
-				System.out.println(lock.get(++count) +""+ nums.get(count));
-			else
-				System.out.println(lock.get(count) +""+ nums.get(count));
-	    	
-	    
-	    	//Postcondition: prints the bottom of the lock
-
-			for(int i=0; i<10; i++){
-		    		for (int j= i+3; j>0; j--){
-					System.out.print(" ");
-		    		}
-		    		if(i!=9)
-					System.out.print(lock.get(--count2));
-		    		for (int j=18-(2*i); j>1;j--){
-					System.out.print(" ");
-		    		}
-		    		System.out.println(lock.get(++count));
-			}
-			System.out.println("            "+nums.get(count));
+		System.out.println("            "+nums.get(head));
 }
 
 	public void turn(boolean turn, int amt){
-		whichway=turn;
 		buildLock(false);
 		if(turn){
 			while(nums.getFirst()!=amt){
@@ -90,12 +82,12 @@ public class CombinationLock {
 				lock.addLast('+');
 			}
 		}
-		displayLock();
+		displayLock(turn);
 	}
 	
 	public static void main(String[] args){
 		CombinationLock hi=new CombinationLock();
-		hi.turn(false, 4);
-		hi.turn(false, 8);
+		hi.turn(true, 38);
+		hi.turn(true, 25);
 	}
 }
