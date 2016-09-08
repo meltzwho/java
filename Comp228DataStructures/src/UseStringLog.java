466c3451a47b78a615d97f23d5864a5c10576622
meltzwho <meltzer.eric@live.com>
2016-09-08 13:10:16 -0400

    changed comments

--- Comp228DataStructures/src/UseStringLog.java
+++ Comp228DataStructures/src/UseStringLog.java
@@ -1,3 +1,4 @@
+//Modified by: Eric Meltzer
 //----------------------------------------------------------------------------
 // UseStringLog.java           by Dale/Joyce/Weems                   Chapter 2
 //
@@ -25,4 +26,20 @@ public class UseStringLog
     System.out.println("The Empire Strikes Back is in the log: " + Movies.contains("The Empire Strikes Back"));
     System.out.println("Rosencrantz & Guildenstern Are Dead is in the log: " + Movies.contains("Rosencrantz & Guildenstern Are Dead"));    
   }
+  /*
+Log: Movies
+
+1. The Bourne series
+2. Taken
+3. Charlie and the Chocolate Factory (Gene Wilder - 1970 version)
+4. Sausage Party
+5. Fast and Furious
+6. The Girl with the Dragon Tattoo (Swedish Original series)
+7. Forrest Gump?
+
+The size of the log is 7
+The Great Escape is in the log: false
+The Empire Strikes Back is in the log: false
+Rosencrantz & Guildenstern Are Dead is in the log: false
+*/
 }
